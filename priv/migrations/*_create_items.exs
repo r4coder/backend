ef701f2f241df1f847ce5e4b523dfba1054d9835
r4coder <ritheeshk2003@gmail.com>
2026-01-04 11:50:31 +0530

    Create migration for items table with unique SKU

---
+++ priv/migrations/*_create_items.exs
@@ -0,0 +1,14 @@
+defmodule InventoryApi.Repo.Migrations.CreateItems do
+  use Ecto.Migration
+
+  def change do
+    create table(:items) do
+      add :name, :string, null: false
+      add :sku, :string, null: false
+      add :unit, :string, null: false
+      timestamps()
+    end
+
+    create unique_index(:items, [:sku])
+  end
+end
